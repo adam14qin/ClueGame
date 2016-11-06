309e3c8030cbfd0a0c7bdf1b85f0e02702f06ce3
CHR <CHR>
2016-11-05 23:58:56 -0600

    added menu bar and detectiveNotes class

--- src/clueGame/ClueGame.java
+++ src/clueGame/ClueGame.java
@@ -1,6 +1,9 @@
 package clueGame;
 
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
 public class ClueGame extends JFrame{
 
@@ -16,6 +19,24 @@ public class ClueGame extends JFrame{
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	
 		setResizable(true);
 		add(board);
+		JMenu menu=new JMenu("File");
+		JMenuBar menuBar=new JMenuBar();
+		setJMenuBar(menuBar);
+		menu.add(createDetectiveNotes());
+		menu.add(creatFileExitItem());
+		menuBar.add(menu);
+	}
+	
+	private JMenuItem creatFileExitItem()
+	{
+		JMenuItem item=new JMenuItem("Exit");
+		return item;
+	}
+	
+	private JMenuItem createDetectiveNotes()
+	{
+		JMenuItem item=new JMenuItem("new detective note");
+		return item;
 	}
 	
 	public static void main(String[] args) {
